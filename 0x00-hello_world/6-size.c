23bef60e5a14c544a64f3a8c81e6bd4d68bb16dc
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-08-15 02:51:24 +0300

    edit: 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	puts("Size of a char: 1 byte(s)");
-	puts("Size of an int: 4 byte(s)");
-	puts("Size of a long int: 4 byte(s)");
-	puts("Size of a long long int: 8 byte(s)");
-	puts("Size of a float: 4 byte(s)");
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
 	return (0);
 }
