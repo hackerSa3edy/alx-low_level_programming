80c304d3f64069de24dfddff7ea276c750e37327
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-08-15 23:43:40 +0300

    added: 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * main - Entry Point
+ *
+ * writing to standard error
+ *
+ * Return: This time will fail
+ */
+int main(void)
+{
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, message, strlen(message));
+	return (1);
+}
