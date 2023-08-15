128d9718bbd8557c05400a87ba8a11acf6a2e12b
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-08-15 05:19:30 +0300

    added: 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 97;
+	int C = 65;
+
+	while (c <= 122)
+	{
+		putchar(c);
+		++c;
+	}
+
+	while (C <= 90)
+	{
+		putchar(C);
+		++C;
+	}
+	putchar(10);
+	return (0);
+}
