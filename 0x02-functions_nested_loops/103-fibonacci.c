804c0f2acdfe9cc629aae863cdacd66a3f005e05
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-08-17 23:11:55 +0300

    added: 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers.
+ *
+ * Return: Always success (0).
+ */
+int main(void)
+{
+	long int firstNum = 0;
+	long int secondNum = 1;
+	long int sum = 0;
+	long int evenSum = 0;
+
+	while (sum <= 4000000)
+	{
+		sum = firstNum + secondNum;
+		firstNum = secondNum;
+		secondNum = sum;
+		if (sum % 2 == 1)
+		{
+			evenSum = evenSum + sum;
+		}
+	}
+	printf("%ld\n", evenSum);
+	return (0);
+}
