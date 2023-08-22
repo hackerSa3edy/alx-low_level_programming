b0d6f7b5c9b0bbf302784434325eaf13967756e8
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-08-22 05:22:02 +0300

    edit+2: 6-puts.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,13 +10,22 @@
 void puts2(char *str)
 {
 	char *ptr;
+	int size = 0;
+	int counter = 0;
 
 	ptr = str;
-	while ((*ptr != '\0') || (*str != '\0'))
+	while (*ptr != '\0')
+	{
+		size = size + sizeof(*ptr);
+		ptr = ptr + 1;
+	}
+
+	ptr = str;
+	while ((counter <= size) && (*ptr != '\0'))
 	{
 		_putchar(*ptr);
 		ptr = ptr + 2;
-		str = str + 1;
+		counter = counter + 2;
 	}
 	_putchar('\n');
 }
