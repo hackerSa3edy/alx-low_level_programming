2d7c6bebe82f7f0daeca1a1ad13804057160f10e
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-08-23 03:47:24 +0300

    added: 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+/**
+ * _isdigit - checks for a digit (0 through 9).
+ *
+ * @c: char to be checked
+ *
+ * Return: 1 if @c is digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+/**
+ * _atoi - convert a string to an integer.
+ *
+ * @s: string to be converted.
+ *
+ * Return: the converted number.
+ */
+int _atoi(char *s)
+{
+	char *ptr;
+	int size, sign, number, for_counter, num_size;
+
+	size = number = 0;
+	sign = 1;
+	ptr = s;
+	while (*ptr != '\0')
+	{
+		if (*ptr == '-')
+		{
+			sign = sign * -1;
+			ptr = ptr + 1;
+			continue;
+		}
+		else if (*ptr == '+')
+		{
+			ptr = ptr + 1;
+			continue;
+		}
+
+		if (_isdigit(*ptr))
+		{
+			size = size + 1;
+			if ((*(ptr + 1) == ' ') || (*(ptr + 1) == '\0'))
+			{
+				num_size = size - 1;
+				for (for_counter = 0; for_counter < size; for_counter++)
+				{
+					number = number * 10 + (*(ptr - num_size) - '0');
+					num_size--;
+				}
+				return (number * sign);
+			}
+		}
+		ptr = ptr + 1;
+	}
+	printf("\n\n");
+	return (0);
+}
