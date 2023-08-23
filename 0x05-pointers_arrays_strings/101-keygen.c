4535ca8b7c7544c08dcae113dd31392328f1eda4
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-08-23 18:02:50 +0300

    added: 101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * calculateChecksum - Calc checksum for a random string.
+ *
+ * @str: random string.
+ *
+ * Return: Checksum.
+ */
+int calculateChecksum(char *str)
+{
+	int checksum, i;
+
+	checksum = i = 0;
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		checksum += str[i];
+	}
+	return (checksum);
+}
+
+/**
+ * main - print random pass which its checksum equlas 2772.
+ *
+ * Return: Always Success (0).
+ */
+int main(void)
+{
+	int desiredChecksum = 2772;
+	int checksum = 0;
+	int length;
+	char random_char[44];
+
+	srand(time(0));
+	while (checksum != desiredChecksum)
+	{
+		for (length = 0; length < 43; length++)
+		{
+			random_char[length] = (rand() % 26) + 65;
+			random_char[length + 1] = '\0';
+			checksum = calculateChecksum(random_char);
+			if (checksum == desiredChecksum)
+			{
+				printf("%s\n", random_char);
+				break;
+			}
+
+		}
+	}
+	return (0);
+}
