b9dea9c61e65e438756df1fa15c0a81148a77586
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-08-24 22:03:40 +0300

    added: 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * string_toupper - convert all lowercase letters to uppercace.
+ *
+ * @str: string to be converted.
+ *
+ * Return: pointer to str.
+ */
+char *string_toupper(char *str)
+{
+	char upper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char lower[] = "abcdefghijklmnopqrstuvwxyz";
+	int index = 0;
+	int index2 = 0;
+
+	while (str[index] != '\0')
+	{
+		if ((str[index] >= 98) && (str[index] <= 122))
+		{
+			while (lower[index2] != '\0')
+			{
+				if (lower[index2] == str[index])
+				{
+					str[index] = upper[index2];
+					break;
+				}
+				index2++;
+			}
+			index2 = 0;
+		}
+		index++;
+	}
+	return (str);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@ int _putchar(char);
 char *_strcat(char *, char *);
 char *_strncat(char *, char *, int);
 char *_strncpy(char *, char *, int);
+char *string_toupper(char *);
 
 #endif /* _MAIN_H_ */
