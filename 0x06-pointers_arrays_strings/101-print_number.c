c820c2fc46d62bad3b5b2eca2b0d81c2aec8b1da
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-08-25 04:28:45 +0300

    added: 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,12 +11,22 @@ void print_number(int n)
 {
 	int singleDigit;
 	int base = 10;
+	int sign = 1;
 
+	if (n < 0)
+	{
+		n = -n;
+		sign = -1;
+	}
 	while (n >= base)
 	{
 		base = base * 10;
 	}
 
+	if (sign == -1)
+	{
+		_putchar('-');
+	}
 	while (base != 1)
 	{
 		singleDigit = (n % base) / (base / 10);
