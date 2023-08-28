2e0956c6a140d966438f16583b1755d647a309ec
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-08-28 20:24:28 +0300

    added: 5-strstr.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ char *_memcpy(char *, char*, unsigned int);
 char *_strchr(char *, char);
 unsigned int _strspn(char *, char *);
 char *_strpbrk(char *, char *);
+char *_strstr(char *, char *);
 
 #endif /* _MAIN_H_ */
