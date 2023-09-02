fa31e3b0cd4338247914ca078aa34fc76360d407
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-09-02 03:46:05 +0300

    edit: 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,8 @@
  *
  * Return: Always success (0).
  */
-int main(int argc, char **argv)
+int main(int argc __attribute__((unused)), char **argv)
 {
-	(void)argc;
 	printf("%s\n", *argv);
 	return (0);
 }
