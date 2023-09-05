278c54383a0c737c2d00cd0bd7f4e8b6e470d542
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-09-05 10:45:51 +0300

    edit: 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -5,15 +5,25 @@
 /**
  * str_concat - concatenates two strings.
  *
- * @string1: first string.
- * @string2: second string.
+ * @str1: first string.
+ * @str2: second string.
  *
  * Return: pointer to the newly string.
  */
-char *str_concat(char *string1, char *string2)
+char *str_concat(char *str1, char *str2)
 {
 	unsigned int index1, index2, counter;
-	char *ptr;
+	char *ptr, *string1, *string2;
+
+	if (str1 == NULL)
+		string1 = "";
+	else
+		string1 = str1;
+
+	if (str2 == NULL)
+		string2 = "";
+	else
+		string2 = str2;
 
 	for (index1 = 0; string1[index1];)
 		index1++;
@@ -21,7 +31,7 @@ char *str_concat(char *string1, char *string2)
 	for (index2 = 0; string2[index2];)
 		index2++;
 
-	ptr = malloc((index1 + index2 - 1) * sizeof(char));
+	ptr = malloc((index1 + index2 + 1) * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
 
