3c48e8618fe08f9d4bc64f1d786e9551a99475bf
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-09-12 04:42:43 +0300

    edit+3: 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 
 #define FUNCTION_LIKE_MACRO_H
-#include <limits.h>
-#define ABS(x) (x >= 0 ? x : (x == INT_MIN ? -(x + 1) : -x))
+#define ABS(x) ((x >= 0) ? (x) : (-x))
 
 #endif /* FUNCTION_LIKE_MACRO_H */
