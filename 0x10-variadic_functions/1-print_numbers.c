cc924d7ad9eb433a7a8611c076d68ddea929edcc
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-09-15 00:25:02 +0300

    add edge case

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,7 +14,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int index;
 	va_list ptr;
 
-	if (separator == NULL)
+	if (separator == NULL || n == 0)
 		return;
 
 	va_start(ptr, n);
