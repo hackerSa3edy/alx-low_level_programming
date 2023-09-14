5a3ffcc4bb717336e8352eded2afa1e6cf5ff1bb
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-09-15 00:28:01 +0300

    fix bug

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,7 +14,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int index;
 	va_list ptr;
 
-	if (separator == NULL || n == 0)
+	if (separator == NULL)
 		return;
 
 	va_start(ptr, n);
@@ -24,6 +24,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (index != n - 1)
 			printf("%s", separator);
 	}
+	va_end(ptr);
 
 	printf("\n");
 }
