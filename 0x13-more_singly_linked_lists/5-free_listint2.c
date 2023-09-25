bb1ee96a8389104d3da1182597a6c19013d98c90
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-09-25 09:30:42 +0300

    task six: 5-free_listint2.c. status: new.

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free a listint_t list and sets the head to null.
+ *
+ * @head: head node.
+ *
+ * Return: Nothing.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr = *head;
+
+	while (*head != NULL)
+	{
+		ptr = *head;
+		*head = (*head)->next;
+		free(ptr);
+	}
+}
