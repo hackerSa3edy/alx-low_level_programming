ed98e87063592c2622215db76b50957f384c46b3
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-09-26 02:02:40 +0300

    task ten: 9-insert_nodeint.c - status: edit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,9 @@
 #include "lists.h"
 
 listint_t *add_node(listint_t *, const int, listint_t *);
+size_t list_len(const listint_t *);
+listint_t *insert_between(listint_t *head, listint_t *newNode,
+		unsigned int place);
 
 /**
  * insert_nodeint_at_index - insert a new node at a given position.
@@ -14,7 +17,7 @@ listint_t *add_node(listint_t *, const int, listint_t *);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *newNode = NULL, *temp = NULL;
-	unsigned int counter = 0;
+	unsigned int len;
 
 	if (head != NULL)
 	{
@@ -23,9 +26,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (newNode == NULL)
 			return (NULL);
 
+		len = list_len(*head);
 		temp = *head;
 		newNode->n = n;
-		if (temp == NULL)
+		if (len == 0)
 		{
 			newNode->next = NULL;
 			*head = newNode;
@@ -35,29 +39,50 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (idx == 0)
 			return (add_node(temp, n, newNode));
 
-		while (temp->next != NULL && counter < idx - 1)
+		if (len == idx)
 		{
-			counter++;
-			temp = temp->next;
+			free(newNode);
+			return (add_nodeint_end(head, n));
 		}
-
-		if (counter == idx - 1)
-		{
-			newNode->next = temp->next;
-			temp->next = newNode;
-		}
-
-		if ((idx - counter) > 1 && temp->next == NULL)
+		else if (len < idx)
 		{
 			free(newNode);
 			return (NULL);
 		}
+		else
+			return (insert_between(*head, newNode, idx));
 	}
 	return (newNode);
 }
 
 /**
- * add_nodeint - adds a new node at the beginning of a listint_t list.
+ * insert_between - inserts new node between two nodes.
+ *
+ * @head: head node.
+ * @newNode: new node to be inserted.
+ * @place: the index of the place to insert the new node at.
+ *
+ * Return: the address of the new node.
+ */
+listint_t *insert_between(listint_t *head, listint_t *newNode,
+		unsigned int place)
+{
+	listint_t *temp = head;
+	unsigned int index = 1;
+
+	while (index < place)
+	{
+		temp = temp->next;
+		index++;
+	}
+	newNode->next = temp->next;
+	temp->next = newNode;
+
+	return (newNode);
+}
+
+/**
+ * add_node - adds a new node at the beginning of a listint_t list.
  *
  * @head: head node.
  * @n: data field.
@@ -71,4 +96,23 @@ listint_t *add_node(listint_t *head, const int n, listint_t *newNode)
 	newNode->next = head;
 	head = newNode;
 	return (newNode);
+}
+
+/**
+ * list_len - calculates the number of elements in a linked listint_t list.
+ *
+ * @h: head node.
+ *
+ * Return: size of the list.
+ */
+size_t list_len(const listint_t *h)
+{
+	size_t len = 0;
+	const listint_t *ptr = h;
+
+	for (; ptr != NULL; ptr = ptr->next)
+	{
+		len++;
+	}
+	return (len);
 }
