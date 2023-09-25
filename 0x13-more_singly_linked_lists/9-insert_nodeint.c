69ab8ee6df448616f9835698215dd8494f65eda3
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-09-26 00:54:00 +0300

    task ten: 9-insert_nodeint.c - status: edit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,7 @@
 #include "lists.h"
 
+listint_t *add_node(listint_t *, const int, listint_t *);
+
 /**
  * insert_nodeint_at_index - insert a new node at a given position.
  *
@@ -11,38 +13,62 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *newNode = malloc(sizeof(listint_t));
-	listint_t *temp;
+	listint_t *newNode = NULL, *temp = NULL;
 	unsigned int counter = 0;
 
-	if (newNode == NULL || head == NULL)
-		return (NULL);
+	if (head != NULL)
+	{
+		newNode = malloc(sizeof(listint_t));
 
-	temp = *head;
+		if (newNode == NULL)
+			return (NULL);
+
+		temp = *head;
+		newNode->n = n;
+		if (temp == NULL)
+		{
+			newNode->next = NULL;
+			*head = newNode;
+			return (newNode);
+		}
+
+		if (idx == 0)
+			return (add_node(temp, n, newNode));
+
+		while (temp->next != NULL && counter < idx - 1)
+		{
+			counter++;
+			temp = temp->next;
+		}
+
+		if (counter == idx - 1)
+		{
+			newNode->next = temp->next;
+			temp->next = newNode;
+		}
+
+		if ((idx - counter) > 1 && temp->next == NULL)
+		{
+			free(newNode);
+			return (NULL);
+		}
+	}
+	return (newNode);
+}
+
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list.
+ *
+ * @head: head node.
+ * @n: data field.
+ * @newNode: node to be added.
+ *
+ * Return: the address of the new element, of NULL if it failed.
+ */
+listint_t *add_node(listint_t *head, const int n, listint_t *newNode)
+{
 	newNode->n = n;
-	if (temp == NULL)
-	{
-		newNode->next = NULL;
-		*head = newNode;
-	}
-
-	while (temp->next != NULL && counter < idx - 1)
-	{
-		counter++;
-		temp = temp->next;
-	}
-
-	if (counter == idx - 1)
-	{
-		newNode->next = temp->next;
-		temp->next = newNode;
-	}
-
-	if ((idx - counter) > 1 && temp->next == NULL)
-	{
-		free(newNode);
-		return (NULL);
-	}
-
+	newNode->next = head;
+	head = newNode;
 	return (newNode);
 }
