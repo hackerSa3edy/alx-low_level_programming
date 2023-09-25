2f0c5ee8c778668e335bbd21f3f02d6789181220
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-09-25 18:56:49 +0300

    task six: 5-free_listint2.c - status: edit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,13 +11,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *ptr = *head;
 
-	if (head == NULL && *head == NULL)
-		return;
-
 	while (*head != NULL)
 	{
 		ptr = *head;
 		*head = (*head)->next;
 		free(ptr);
 	}
+	*head = NULL;
 }
