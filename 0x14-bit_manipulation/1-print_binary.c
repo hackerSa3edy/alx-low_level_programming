bbd3e97c83639ed9613a70dd194af5ca6ffe6b7a
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-09-28 18:37:25 +0300

    task two: 1-print_binary.c - status: new

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number.
+ *
+ * @decimal: decimal number.
+ *
+ * Return: Nothing.
+ */
+void print_binary(unsigned long int decimal)
+{
+	int digit = 1;
+	static int recursive = 0;
+
+	if (decimal == 0 && recursive == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	recursive = 1;
+	if (decimal != 0)
+	{
+		print_binary(decimal >> 1);
+		_putchar((digit & decimal) + '0');
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 
 #include <stdio.h>
+int _putchar(int);
 unsigned int binary_to_uint(const char *);
+void print_binary(unsigned long int);
 
 #endif /* MAIN_H */
