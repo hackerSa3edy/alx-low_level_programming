50f25e7b1c2ddffe11a748e410acff4c4588cb54
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-09-28 18:50:26 +0300

    task two: 1-print_binary.c - status: edit - desc: explain a statement

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,14 +10,15 @@
 void print_binary(unsigned long int decimal)
 {
 	int digit = 1;
-	static int recursive = 1;
+	static int recursive;
 
-	if (decimal == 0 && recursive == 1)
+	/* Static int variables are automatically initialized to 0 by default*/
+	if (decimal == 0 && recursive == 0)
 	{
 		_putchar('0');
 		return;
 	}
-	recursive = 0;
+	recursive = 1;
 	if (decimal != 0)
 	{
 		print_binary(decimal >> 1);
