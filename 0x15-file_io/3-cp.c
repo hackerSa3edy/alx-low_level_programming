197788ae310f7406e3bde2c59eda9039cbd0866f
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-10-03 23:30:07 +0300

    task four: 3-cp.c - status: new

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,77 @@
+#include "main.h"
+
+ssize_t cp(const char *file_from, const char *file_to);
+
+/**
+ * main - copies the content of a file to another file.
+ *
+ * @argc: number of arguments.
+ * @argv: arguments values.
+ *
+ * Return: (1) always success, otherwise EXIT_CODE.
+ */
+int main(int argc, char **argv)
+{
+	if (argc < 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	cp(argv[1], argv[2]);
+
+	return (1);
+}
+
+/**
+ * cp - copies the content of a file to another file.
+ *
+ * @file_from: arguments values.
+ * @file_to: number of arguments.
+ *
+ * Return: (1) always success, otherwise EXIT_CODE.
+ */
+ssize_t cp(const char *file_from, const char *file_to)
+{
+	int f_from_fd, fread, f_to_fd, perm, flags;
+	char *buffer;
+
+	flags = O_TRUNC | O_WRONLY | O_CREAT;
+	perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+
+	f_from_fd = open(file_from, O_RDONLY);
+	buffer = malloc(BUFFER_SIZE);
+	fread = read(f_from_fd, buffer, BUFFER_SIZE);
+	if (f_from_fd < 0 || buffer == NULL || fread < 0)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	f_to_fd = open(file_to, flags, perm);
+	if (f_to_fd < 0)
+	{
+		dprintf(2, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+
+	while (fread != 0)
+	{
+		write(f_to_fd, buffer, fread);
+		fread = read(f_from_fd, buffer, BUFFER_SIZE);
+	}
+
+	free(buffer);
+	if (close(f_from_fd) < 0)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", f_from_fd);
+		exit(100);
+	}
+	if (close(f_to_fd) < 0)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", f_to_fd);
+		exit(100);
+	}
+
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,9 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
+
+#define BUFFER_SIZE 1024
 
 ssize_t read_textfile(const char *, size_t);
 int create_file(const char *, char *);
