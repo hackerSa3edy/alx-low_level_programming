16e48f3fa09d4e67094fbf36d9a8b6fe92302903
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-10-06 12:40:49 +0300

    task five: 100-elf_header.c - status: edit - desc: remove unnecassary code.

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -90,12 +90,6 @@ void checkELF(unsigned char *e_ident, Elf64_Ehdr *header, int fd)
 		close(fd);
 		exit(98);
 	}
-	if (e_ident[EI_CLASS] != ELFCLASS32 && e_ident[EI_CLASS] != ELFCLASS64)
-	{
-		free(header);
-		close(fd);
-		exit(98);
-	}
 }
 
 /**
