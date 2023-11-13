65251eeca9ab5f7d6f918cac0b28c4d0e0d59757
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-11-13 23:38:43 +0200

    Remove comments

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Create unlinked Position-Independent Code file
 gcc -std=gnu89 -Wextra -Werror -Wall -pedantic -c -fPIC *.c
-
-# Create the shared library (dynamic library)
 gcc -std=gnu89 -Wextra -Werror -Wall -pedantic -shared *.o -o liball.so
