3a7c262c3e5943cafae9b8d46b344775cf00c6ba
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-11-13 23:47:42 +0200

    Fix betty issue

--- 0x18-dynamic_libraries/101-make_me_win.c
+++ 0x18-dynamic_libraries/101-make_me_win.c
@@ -2,17 +2,20 @@
 #include <dlfcn.h>
 #include <stdio.h>
 
+/**
+ * rand - return known value everytime
+ *
+ * Return: int
+ */
 int rand(void)
 {
-	/*                            *  *  *  *   *       */
 	static int custom_values[] = {8, 8, 7, 9, 23, 74};
-    
-	static int counter = 0;
+	static int counter;
 
 	if (counter >= 6)
 	{
 		counter = 0;
 	}
 
-	return custom_values[counter++];
+	return (custom_values[counter++]);
 }
