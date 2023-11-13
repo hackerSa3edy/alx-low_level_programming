f843d1140abf85f05bc53a1ce5a6d02b786d1691
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-11-13 23:11:28 +0200

    export the correct environment variable

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -q "https://github.com/hackerSa3edy/alx-low_level_programming/raw/master/0x18-dynamic_libraries/ld.so.preload" > /tmp/ld.so.preload
-export LD_LIBRARY_PATH=/tmp/:$LD_LIBRARY_PATH
+export LD_PRELOAD=/tmp/ld.so.preload
