7b9f9e624cf6c3360ac2e13158fd7b807e4c5bba
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-11-13 23:01:27 +0200

    change output redirect method

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget "https://github.com/hackerSa3edy/alx-low_level_programming/raw/master/0x18-dynamic_libraries/ld.so.preload" -O /etc/ld.so.preload
+wget -q "https://github.com/hackerSa3edy/alx-low_level_programming/raw/master/0x18-dynamic_libraries/ld.so.preload" > /etc/ld.so.preload
 export LD_LIBRARY_PATH=/etc/:$LD_LIBRARY_PATH
