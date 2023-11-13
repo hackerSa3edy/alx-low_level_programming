8977fd898b09ccbcff70325c05a1d1992dac0df6
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2023-11-13 18:33:52 +0200

    Add task three: 100-operations.so

---
+++ 0x18-dynamic_libraries/3-op_functions.c
@@ -0,0 +1,64 @@
+/**
+ * add - add two numbers.
+ *
+ * @a: num1
+ * @b: num2
+ *
+ * Return: num1 + num2
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two numbers.
+ *
+ * @a: num1
+ * @b: num2
+ *
+ * Return: num1 - num2
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply two numbers.
+ *
+ * @a: num1
+ * @b: num2
+ *
+ * Return: num1 * num2
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide two numbers.
+ *
+ * @a: num1
+ * @b: num2
+ *
+ * Return: num1 / num2
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modules a number.
+ *
+ * @a: num1
+ * @b: num2
+ *
+ * Return: num1 % num2
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
