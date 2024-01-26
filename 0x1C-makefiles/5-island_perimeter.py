927f7b00c5f7e1d968a4fa0774683dee1df2aace
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-01-26 22:13:29 +0200

    Add Makefile tasks' solutions

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Calculates the perimeter of the island described in grid
+"""
+
+
+def island_perimeter(grid):
+    """island_perimeter
+    desc:
+        * grid is a list of list of integers:
+            - 0 represents a water zone
+            - 1 represents a land zone
+            - One cell is a square with side length 1
+            - Grid cells are connected horizontally/vertically
+                (not diagonally).
+            - Grid is rectangular, width and height don’t exceed 100
+            - Grid is completely surrounded by water, and there is
+                one island (or nothing).
+            - The island doesn’t have “lakes” (water inside that isn’t
+                connected to the water around the island).
+
+    return:
+        The perimeter of the island described in grid
+    """    
+
+    return perimeter
